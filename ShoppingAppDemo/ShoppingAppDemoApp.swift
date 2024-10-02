f23e8de0aee8aa9692d96ff29405272723d195d8
olivier <olivierjacquet333@gmail.com>
2024-10-02 12:49:35 +0400

    shopping app demo

---
+++ ShoppingAppDemo/ShoppingAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingAppDemoApp.swift
+//  ShoppingAppDemo
+//
+//  Created by Stellone Olivier on 02/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
